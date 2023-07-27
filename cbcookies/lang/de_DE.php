9fd4ec55dd7f3f7ce50d461680163b3a471c8b0c
VeNickov <78678788+VeNickov@users.noreply.github.com>
2023-07-27 11:05:36 +0300

    Add files via upload

---
+++ cbcookies/lang/de_DE.php
@@ -0,0 +1,30 @@
+<?php
+//set internationalization
+
+        $i18n = array (
+        "name"=> 'cbCookies',
+        "message0" =>  'Wir nutzen eigene oder Cookies Dritter um Ihre Surferfahrung zu verbessern. Durch Nutzung unseres Angebots gehen wir davon aus, dass Sie der Nutzung derer zustimmen.',
+        "message1" =>  'Durch Nutzung dieser Webseite stimmen Sie der Verwendung von Cookies zu Zwecken der Analyse, zur Bereitstellung von personalisierten Inhalten und Werbung zu.',
+        "message2" =>  'Cookies helfen uns, Ihnen unsere Dienstleistung zu erbringen. Wenn Sie unsere Dienstleistung in Anspruch nehmen, stimmen Sie der Verwendung von Cookies zu.',
+        "accept" => 'Akzeptieren',
+        "accept1" => 'X',
+        "more_in" => 'Weitere Informationen',
+
+        //BACKEND
+        "setting" => 'CBCOOKIES Einstellungen',
+        "page_more" => 'Seite, auf der weitere Informationen zur Nutzung von Cookies angezeigt wird (z.B. Datenschutzerklärung)',
+        "message_lb" => 'Text der angezeigt wird, um den Nutzer über den Einsatz von Cookies zu informieren',
+        "pos_message" => 'Position vom Text',
+	"time_cookie" => 'Cookies verfallen nach',
+	"cbtop" => 'Oben',
+	"cbbottom" => 'Unten',
+	"cbscale" => 'Einheit',
+	"cbdays" => 'Tage',
+	"cbhours" => 'Stunden',
+	"cbminutes" => 'Minuten',
+	"cbseconds" => 'Sekunden',
+	"cbSave" => 'Speichern',
+	"cbchsav" => 'Änderungen wurden gespeichert'
+
+        )
+?>
---
+++ cbcookies/lang/el_GR.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Greek Language File
+ *
+ * Date:			4 December 2013
+ * Revision:		4 December 2013
+ * Version:			CBCookies v1.0
+ * Traductors: 		Dimitris Papadimitriou
+ *
+ * @package CBCookies
+ * @subpackage Language
+ */
+
+        $i18n = array (
+        "name"=> 'cbCookies',
+        "message0" =>  'We use our own or third party cookies to improve your web browsing experience. If you continue to browse we consider that you accept their use.',
+        "message1" =>  'By using this site you accept the use of cookies for analysis, customized content and advertising.',
+        "message2" =>  'Cookies allow us to offer our services. By using our services, you accept our use of cookies.',
+        "accept" => 'Accept',
+        "accept1" => 'X',
+        "more_in" => 'More information',
+
+        //BACKEND
+        "setting" => 'Ρυθμίσεις CBCOOKIES',
+        "page_more" => 'Σελίδα που θα δείχνουμε την προειδοποίηση',
+        "message_lb" => 'Μήνυμα προειδοποίησης χρήσης Cookies',
+        "pos_message" => 'Εμφάνιση μηνύματος στο ____ μέρος της σελίδας',
+	"time_cookie" => 'Το Cookie να λήξει μετά απο',
+	"cbtop" => 'Πάνω',
+	"cbbottom" => 'Κάτω',
+	"cbscale" => 'Μονάδα μέτρησης χρόνου',
+	"cbdays" => 'Ημέρες',
+	"cbhours" => 'Ώρες',
+	"cbminutes" => 'Λεπτά',
+	"cbseconds" => 'Δευτερόλεπτα',
+	"cbSave" => 'Αποθήκευση',
+	"cbchsav" => 'Οι αλλαγές αποθηκευτηκαν'
+
+        )
+?>
